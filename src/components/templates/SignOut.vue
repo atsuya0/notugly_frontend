532156dd9ba82617a67a8f9e6054f86725b43122
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-02 11:56:38 +0900

    [add] SignOutするだけのページ

---
+++ src/components/templates/SignOut.vue
@@ -0,0 +1,13 @@
+<template
+  ><div></div
+></template>
+
+<script>
+export default {
+  name: "SignOut",
+  created: function() {
+    this.$store.dispatch("signOut");
+    this.$router.push("/signin");
+  }
+};
+</script>
