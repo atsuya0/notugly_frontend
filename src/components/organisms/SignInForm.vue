a1828e58cb20e9f1a2ccebaae3585a2490dd8740
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-02 11:57:27 +0900

    [add] SignInページ

---
+++ src/components/organisms/SignInForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card class="elevation-12">
+    <v-toolbar dark color="primary">
+      <v-toolbar-title>
+        Sign In Form
+      </v-toolbar-title>
+    </v-toolbar>
+    <v-card-text>
+      <v-form ref="form" v-model="valid">
+        <v-text-field
+          v-model="email"
+          prepend-icon="email"
+          name="email"
+          label="E-mail"
+          type="email"
+          :counter="32"
+          :rules="[rules.email, rules.maxLen(32)]"
+          required
+        >
+        </v-text-field>
+        <v-text-field
+          v-model="password"
+          prepend-icon="lock"
+          name="password"
+          label="Password"
+          type="password"
+          :counter="32"
+          :rules="[rules.password, rules.minLen(6), rules.maxLen(32)]"
+          required
+        >
+        </v-text-field>
+      </v-form>
+
+      <router-link to="/signup">sign up now!</router-link>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn flat @click="$refs.form.reset()">
+        Clear
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn
+        color="primary"
+        :loading="isLoading"
+        :disabled="!valid || isLoading"
+        @click="signIn"
+      >
+        Sign In
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import validate from "@/validate.js";
+
+export default {
+  name: "SignInForm",
+  data: () => ({
+    email: "",
+    password: "",
+    valid: false,
+    isLoading: false,
+    rules: validate.rules
+  }),
+  methods: {
+    signIn: async function() {
+      this.isLoading = true;
+      await this.$store.dispatch("signIn", {
+        email: this.email,
+        password: this.password
+      });
+      await this.$router.push("/");
+      this.isLoading = false;
+    }
+  }
+};
+</script>
---
+++ src/components/templates/SignIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <sign-in-form />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import SignInForm from "@/components/organisms/SignInForm.vue";
+
+export default {
+  name: "SignIn",
+  components: {
+    SignInForm
+  }
+};
+</script>
