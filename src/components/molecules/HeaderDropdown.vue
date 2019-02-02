a893c378090e0ccef2f585b8387a1c5da0ecc95e
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-02 11:58:02 +0900

    [add] Header

---
+++ src/components/molecules/HeaderDropdown.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-menu offset-y left transition="slide-y-transition">
+    <v-btn icon slot="activator">
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+    <v-list>
+      <v-list-tile v-for="(item, index) in menu" :key="index">
+        <v-list-tile-title>
+          <router-link tag="div" :to="item.link">
+            {{ item.title }}
+          </router-link>
+        </v-list-tile-title>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  name: "HeaderDropdown",
+  props: {
+    menu: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
---
+++ src/components/organisms/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-toolbar color="primary" app>
+    <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+      <router-link tag="div" to="/">
+        <span class="hidden-sm-and-down">NOTUGLY</span>
+      </router-link>
+    </v-toolbar-title>
+    <v-spacer></v-spacer>
+    <header-dropdown :menu="menu" v-if="!isSignPage()" />
+  </v-toolbar>
+</template>
+
+<script>
+import HeaderDropdown from "@/components/molecules/HeaderDropdown.vue";
+
+export default {
+  name: "Header",
+  components: {
+    HeaderDropdown
+  },
+  data: () => ({
+    menu: [
+      { title: "プロフィール", link: "/profile" },
+      { title: "サインアウト", link: "/signout" }
+    ]
+  }),
+  props: {
+    path: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    isSignPage: function() {
+      if (this.path === "/signup" || this.path === "/signin") {
+        return true;
+      }
+      return false;
+    }
+  }
+};
+</script>
