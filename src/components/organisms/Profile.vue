92669d0101a17265e414c053d1bd1c532a771a40
tayusa <37957375+tayusa@users.noreply.github.com>
2019-02-05 00:08:50 +0900

    [add] Profileコンポーネント

---
+++ src/components/organisms/Profile.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container fluid>
+    <v-card flat>
+      <v-layout column align-center>
+        <v-tooltip top>
+          <v-card-title class="headline" slot="activator">
+            {{ name }}
+          </v-card-title>
+          <span>{{ id }}</span>
+        </v-tooltip>
+        <v-card-actions>
+          <span class="mx-2">{{ $store.state.sexList[sex] }}</span>
+          <span>/</span>
+          <span class="mx-2">{{ age }} years old</span>
+        </v-card-actions>
+      </v-layout>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import userService from "@/api/user";
+
+export default {
+  name: "Profile",
+  data: () => ({
+    id: "",
+    name: "",
+    sex: 0,
+    age: -1
+  }),
+  created: function() {
+    this.fetchUser();
+  },
+  methods: {
+    fetchUser: async function() {
+      const res = await userService
+        .get(this.$store.state.auth.uid)
+        .catch(err => {
+          console.log(err);
+        });
+      this.id = res.data.id;
+      this.name = res.data.name;
+      this.sex = res.data.sex;
+      this.age = res.data.age;
+    }
+  }
+};
+</script>
